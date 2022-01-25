90a356ddae21e42e39f96055a75fdced1e270f1c
ILLUSIONONTOP <98376216+ILLUSIONONTOP@users.noreply.github.com>
2022-01-25 11:54:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -19,4 +19,4 @@ global.player = new Player(client, client.config.opt.discordPlayer);
 require('./src/loader');
 require('./src/events');
 
-client.login("OTM1Mzk4MzY1NzEyMjI0MjU2.Ye-DnQ.L3EcV4Dopm1M5zQssvIzJ450FO4");
+client.login()
